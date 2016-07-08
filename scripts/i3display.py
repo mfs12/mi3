c56c16a8e33f59911a481e147e28cbdc5aa8bd60
biiiep <ukikmes@googlemail.com>
2016-07-08 17:06:06 +0200

    i3display: added monitor for device changes

--- scripts/i3display.py
+++ scripts/i3display.py
@@ -4,8 +4,13 @@ import pyudev
 
 def main():
     context = pyudev.Context()
-    for device in context.list_devices(subsystem="drm", kernel="card0"):
-        print (device)
+    for device in context.list_devices(subsystem="drm"):
+         print('{0} ({1})'.format(device, device.device_type))
+
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem="drm")
+    for device in iter(monitor.poll, None):
+        print('{0} {1}'.format(device.action, device))
 
 if __name__ == "__main__":
         main()
